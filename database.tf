c7c30d092327e9202d009ca65403280a772c5763
James Woolfenden <james.woolfenden@gmail.com>
2021-02-18 11:39:40 +0000

    init

---
+++ database.tf
@@ -0,0 +1,11 @@
+resource "azurerm_mariadb_database" "example" {
+  name                = var.db_name
+  resource_group_name = var.resource_group.name
+  server_name         = azurerm_mariadb_server.example.name
+  charset             = "utf8"
+  collation           = "utf8_general_ci"
+}
+
+variable "db_name" {
+  default="mariadb_database"
+}
---
+++ example/examplea/module.tf
@@ -0,0 +1,5 @@
+module "mariadb" {
+  source      = "../../"
+  common_tags = var.common_tags
+  server_name=  "myfirstmariaserver"
+}
